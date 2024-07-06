a88260b297d0d7f32e0f87f09d0eb28a6dc13f14
FFFF-0000h <adebiyiitunuayo@gmail.com>
2024-07-06 03:58:12 +0100

    Repeat string and test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(strr, num) {
 
+    let stringg = "";
+    if (num < 0) {
+        return "ERROR"; 
+    }
+    
+    for(let i = 0; i < num; i++) {
+        stringg += strr;
+    }
+    
+    return stringg;
 };
 
 // Do not edit below this line
