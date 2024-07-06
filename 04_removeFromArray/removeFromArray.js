316352c1d4c8b144d915784a5ac84e63d2620a9e
FFFF-0000h <adebiyiitunuayo@gmail.com>
2024-07-06 13:15:55 +0100

    Remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(harray, ...args) {
+        // Filter the array to exclude elements present in args
+        return harray.filter(item => !args.includes(item));      
 };
 
 // Do not edit below this line
