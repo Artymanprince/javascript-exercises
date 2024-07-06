485f2d781f1f1e0f495803b53ae3d9536859822e
FFFF-0000h <adebiyiitunuayo@gmail.com>
2024-07-06 13:26:29 +0100

    Calculate the leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+        // A year is a leap year if it is divisible by 4
+        // but not divisible by 100 unless it is also divisible by 400.
+        return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 };
 
 // Do not edit below this line
